381fc53ac363cbde8e9c541ae3edaeb8bbf21636
Talha Ucar <talhamucar@gmail.com>
2019-01-07 21:59:05 +0000

    authentication is added

--- app.js
+++ app.js
@@ -6,10 +6,31 @@ var bodyParser = require("body-parser");
 var Footballer = require("./models/footballer");
 var Comment = require("./models/comment");
 
+//for authantication
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user");
+
 mongoose.connect("mongodb://localhost/footballers_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// passport configuration
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 //routers
 app.get("/", function(req, res){
@@ -67,7 +88,7 @@ app.get("/footballers/:id", function(req, res) {
 //============================
 
 //new
-app.get("/footballers/:id/comments/new", function(req, res) {
+app.get("/footballers/:id/comments/new", isLoggedIn, function(req, res) {
    //find footballer
    Footballer.findById(req.params.id, function(err, footballer){
       if(err){
@@ -79,7 +100,7 @@ app.get("/footballers/:id/comments/new", function(req, res) {
 });
 
 //create
-app.post("/footballers/:id/comments", function(req, res){
+app.post("/footballers/:id/comments", isLoggedIn, function(req, res){
    //find by id
    Footballer.findById(req.params.id, function(err, found) {
        if(err){
@@ -91,11 +112,60 @@ app.post("/footballers/:id/comments", function(req, res){
              } else{
                 found.comments.push(comment);
                 found.save();
+                res.redirect('/footballers/' + found._id);
              }
           });
        }
    })
 });
+
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/footballers"); 
+        });
+    });
+});
+
+// show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+// handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/footballers",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+// logic route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/footballers");
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 //server code
